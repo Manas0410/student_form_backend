644e95f017194a635e07d50257872800e41adc06
Manas0410 <manasshrivastava0410@gmail.com>
2023-10-20 23:59:07 -0700

    cors enable

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 4000;
-
+var cors = require("cors");
+app.use(cors());
 app.use(bodyParser.json());
+
 app.use(express.urlencoded({ extended: false }));
 
 const subjectsList = require("./endPoints.json");
